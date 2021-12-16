f986404dd40158d3e99f5d33bde23df3c934ef3f
hqbobo <29414@qq.com>
2021-12-16 17:18:45 +0800

    up

--- common/weixin/model/msgtemplate.go
+++ common/weixin/model/msgtemplate.go
@@ -1,11 +1,16 @@
 package model
 
+type miniPath struct {
+	Appid    string `json:"appid"`
+	Pagepath string `json:"pagepath"`
+}
 type WeixinTemp struct {
 	ToUser     string      `json:"touser"`
 	TemplateId string      `json:"template_id"`
 	Url        string      `json:"url"`
 	Topcolor   string      `json:"topcolor"`
 	Data       interface{} `json:"data"`
+	Mini       miniPath    `json:"miniprogram"`
 }
 
 type WeixinTempItem struct {
@@ -13,11 +18,10 @@ type WeixinTempItem struct {
 	Color string `json:"color"`
 }
 
-
 type WeixinKfText struct {
 	ToUser  string `json:"touser"`
 	Msgtype string `json:"msgtype"`
 	Text    struct {
-			Content string `json:"content"`
-		} `json:"text"`
+		Content string `json:"content"`
+	} `json:"text"`
 }
